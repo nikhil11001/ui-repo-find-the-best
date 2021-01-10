135ed6e889e5e357331978a0e8deb9b140682096
nikhil11001 <nikhilgade11001@gmail.com>
2021-01-10 21:06:42 +0530

    Quantitiy added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import {
   Button, Navbar, Nav, NavDropdown, Form, FormControl,
-  Row, Col, Image, Badge, ButtonToolbar, ButtonGroup,Jumbotron                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+  Row, Col, Image, Badge, ButtonToolbar, ButtonGroup, Jumbotron
 } from "react-bootstrap";
 
 import image from "./images/shop.png";
@@ -19,8 +19,9 @@ class App extends React.Component {
       locationName: "",
       hotelName: "",
       productName: "",
+      quantity: 1,
       searchResult: [],
-      sellingPartnerList:[],
+      sellingPartnerList: [],
       hotelData: [
         {
           name: "Hotel 1",
@@ -43,7 +44,8 @@ class App extends React.Component {
               name: "Food Panda",
               price: "Rs.90",
             }
-          ]
+          ],
+          quantity: ""
         },
         {
           name: "Hotel 2",
@@ -66,7 +68,8 @@ class App extends React.Component {
               name: "Food Panda",
               price: "Rs.90",
             }
-          ]
+          ],
+          quantity: ""
         },
 
         {
@@ -90,7 +93,8 @@ class App extends React.Component {
               name: "Food Panda",
               price: "Rs.90",
             }
-          ]
+          ],
+          quantity: ""
         },
 
         {
@@ -114,7 +118,8 @@ class App extends React.Component {
               name: "Food Panda",
               price: "Rs.90",
             }
-          ]
+          ],
+          quantity: ""
         },
 
         {
@@ -138,7 +143,8 @@ class App extends React.Component {
               name: "Food Panda",
               price: "Rs.90",
             }
-          ]
+          ],
+          quantity: ""
         },
 
         {
@@ -162,7 +168,8 @@ class App extends React.Component {
               name: "Food Panda",
               price: "Rs.90",
             }
-          ]
+          ],
+          quantity: ""
         }
 
       ]
@@ -186,7 +193,7 @@ class App extends React.Component {
       locationName: "",
       hotelName: "",
       productName: "",
-      searchResult:[]
+      searchResult: []
     }, () => {
       console.log("state--->", this.state)
     })
@@ -198,34 +205,49 @@ class App extends React.Component {
       return (item.name === hotelName || item.location === locationName || item.product === productName)
     })
     console.log("searchRsult--->", searchResult)
-    let result=this.state.hotelData.filter((obj)=>{
-       return obj.name===this.state.hotelName
-    }) 
-    this.setState({ searchResult,sellingPartnerList:result })
+    let result = this.state.hotelData.filter((obj) => {
+      return obj.name === this.state.hotelName
+    })
+    this.setState({ searchResult, sellingPartnerList: result })
   }
 
 
-  getImage=(name)=>{
-    let src="";
-    if(name==="Uber Eats"){
-      src=uberEat;
-    }else if(name==="Food Panda"){
-      src=foodPanda;
-    }else if(name==="Zomato"){
-      src=zomato;
+  getImage = (name) => {
+    let src = "";
+    if (name === "Uber Eats") {
+      src = uberEat;
+    } else if (name === "Food Panda") {
+      src = foodPanda;
+    } else if (name === "Zomato") {
+      src = zomato;
     }
-    return  <Image src={src} roundedCircle fluid className="max-width-50"/>
+
+    return <Image src={src} roundedCircle fluid className="max-width-50" />
   }
 
-  getSellingPartner=()=>{
-  if(this.state.sellingPartnerList.length >0){
-   return( this.state.sellingPartnerList[0].sellingPartner.map((obj)=>{
-      return (
-      <div style={{marginBottom:"20px"}}>{this.getImage(obj.name)}{` ${obj.price}`}</div>
-      );
-  }))
+  getSellingPartner = () => {
+    if (this.state.sellingPartnerList.length > 0) {
+      return (this.state.sellingPartnerList[0].sellingPartner.map((obj) => {
+        return (
+          <div style={{ marginBottom: "20px" }}>{this.getImage(obj.name)}{` ${obj.price}`}</div>
+        );
+      }))
+    }
   }
+
+  updateQuantity = (val, selectedIndex) => {
+
+    let arr = [...this.state.searchResult]
+    arr.map((item, index) => {
+      if (index == selectedIndex) {
+        item.quantity = val
+      }
+
+    })
+    this.setState({ searchResult: arr })
+    console.table(this.state.searchResult)
   }
+
 
   render() {
     return (
@@ -236,14 +258,6 @@ class App extends React.Component {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
             </Nav>
             <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -268,7 +282,7 @@ class App extends React.Component {
                     </Form.Control>
                   </Form.Group>
                 </Col>
-                <Col md={3}>
+                <Col md={2}>
                   <Form.Group controlId="exampleForm.ControlSelect1">
                     <Form.Label>Hotel</Form.Label>
                     <Form.Control as="select"
@@ -291,13 +305,14 @@ class App extends React.Component {
                       value={this.state.productName}
                       onChange={(e) => { this.setState({ productName: e.target.value }) }}>
                       <option>Select Product</option>
-                      <option>Panner Tikka</option>
+                      <option>
+                        Panner Tikka
+                      </option>
                       <option>Veg Pulav</option>
                       <option>Chicken Biryani</option>
                     </Form.Control>
                   </Form.Group>
                 </Col>
-
               </Row>
             </Col>
             <Col md={2} style={{ marginTop: "34px" }}>
@@ -312,65 +327,75 @@ class App extends React.Component {
             </Col>
           </Row>
         </div>
-       
+
         <div style={{ marginLeft: "76px", marginRight: "76px", marginTop: "50px" }}>
-        {this.state.searchResult.length>0 ?<Jumbotron>
-          {
-            this.state.searchResult.map((obj) => {
-              return (
-                <>
-                <Row>
-                  <Col md={6}>
+          {this.state.searchResult.length > 0 ? <Jumbotron>
+            {
+              this.state.searchResult.map((obj, index) => {
+                return (
+                  <>
                     <Row>
-                  <Col md={2}>
-                    <Image src={obj.image} thumbnail />
-                  </Col>
-                  <Col md={6}>
-                    <h4>{obj.name}</h4>
-                    <div>
-                      <Button variant="primary">
-                        <Badge variant="light">{obj.rating}</Badge>
-                        <span className="sr-only">unread messages</span>
-                      </Button>
+                      <Col md={6}>
+                        <Row>
+                          <Col md={2}>
+                            <Image src={obj.image} thumbnail />
+                          </Col>
+                          <Col md={6}>
+                            <h4>{obj.name}</h4>
+                            <div>
+                              <Button variant="primary">
+                                <Badge variant="light">{obj.rating}</Badge>
+                                <span className="sr-only">unread messages</span>
+                              </Button>
 
-                    </div>
-                    <div style={{ marginTop: "30px" }}>
-                      <h5>Info</h5>
-                      <p>
-                        {obj.info}
-                      </p>
-                    </div>
-                  </Col>
-                  </Row>
-                  </Col>
+                            </div>
+                            <div style={{ marginTop: "20px" }}>
+                              <div style={{ display: "flex" }}>
+                                <div>
+                                  <h3>Product:{obj.product}</h3>
+                                </div>
+                                <div style={{ marginLeft: "26px" }}>
+                                  <input type="number" value={obj.quantity} className="form-control" onChange={(e) => { this.updateQuantity(e.target.value, index) }} />
+                                </div>
+                              </div>
+                            </div>
+                            <div style={{ marginTop: "30px" }}>
+                              <h5>Info</h5>
+                              <p>
+                                {obj.info}
+                              </p>
+                            </div>
+                          </Col>
+                        </Row>
+                      </Col>
 
-                  <Col md={6}>
-                    <Row>
-                  <Col md={4}>
-                    <h4>{this.state.hotelName ?this.getSellingPartner() :obj.price}</h4>
-                  </Col>
-                  <Col md={8}>
-                    <Button variant="success">
-                      View Details
+                      <Col md={6}>
+                        <Row>
+                          <Col md={4}>
+                            <h4>{this.state.hotelName ? this.getSellingPartner() : obj.price}</h4>
+                          </Col>
+                          <Col md={8}>
+                            <Button variant="success">
+                              View Details
                     </Button>
-                  </Col>
-                  </Row>
-                  </Col>
-                </Row>
-              <hr/>
-              </>
-              );
-            })
-          }
-          </Jumbotron> : 
-          <Jumbotron>
-            <Image src={foodSerachIcon1} rounded width={"20%"} style={{marginLeft:"40%"}}/>
-            <h1 className="text-center">Find Out Best Food</h1>
-          </Jumbotron>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <hr />
+                  </>
+                );
+              })
+            }
+          </Jumbotron> :
+            <Jumbotron>
+              <Image src={foodSerachIcon1} rounded width={"20%"} style={{ marginLeft: "40%" }} />
+              <h1 className="text-center">Find Out Best Food</h1>
+            </Jumbotron>
           }
         </div>:
-      
-        
+
+
       </div>
     );
   }
